ecb8520cde00e32dad6288eb3fae7ebc5e0d7d53
Erren <775837363@qq.com>
2015-08-26 09:31:11 +0800

    Initial Commit

---
+++ TableView基类继承demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableView基类继承demo
+//
+//  Created by iMac on 15/8/26.
+//  Copyright (c) 2015年 杭州熊管佳电子商务有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
